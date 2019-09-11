5ebdaf502ba07c58485af03ba1bae0256fc12ce7
nadavpriel <nadavpriel@gmail.com>
2019-09-10 17:28:59 -0700

    sdsdsd

--- Tools/StatFramework/Main_simple_sine_fit.py
+++ Tools/StatFramework/Main_simple_sine_fit.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 from likelihood_calculator import likelihood_analyser
+import time
+
+import LeastSquares
 
 print('************************')
 print('Starting simple sine fit')
@@ -10,18 +13,25 @@ lc_i = likelihood_analyser.LikelihoodAnalyser()
 
 # generating fake data
 samples = 5000
-noise_rms = 10
-time = np.arange(0, samples) / 5000
+noise_rms = 1
+time_x = np.arange(0, samples) / 5000
 freq = 100
 amp = 1
-sig_x = amp * np.sin(2 * np.pi * freq * time) + noise_rms * np.random.randn(samples)
+sig_x = amp * np.sin(2 * np.pi * freq * time_x) + noise_rms * np.random.randn(samples)
 
 x = np.random.randn(samples)
 
 # fit arguments
-fit_kwargs = {'A': 5, 'f': 100, 'phi': 0, 'error_A': 2, 'error_f': 10, 'error_phi': 0.5, 'errordef': 0.5,
+fit_kwargs = {'A': 5, 'f': 100, 'phi': 0, 'sigma': 1,
+              'error_A': 2, 'error_f': 10, 'error_phi': 0.5, 'error_sigma': 0.1, 'errordef': 1,
+              'limit_phi': [0, 2 * np.pi],
+              'print_level': 0, 'fix_f': True, 'fix_phi': False, 'fix_sigma': True}
+
+fit_kwargs = {'A': 5, 'f': 100, 'phi': 0,
+              'error_A': 2, 'error_f': 10, 'error_phi': 0.5, 'errordef': 1,
               'limit_phi': [0, 2 * np.pi],
               'print_level': 0, 'fix_f': True, 'fix_phi': False}
 
-m = lc_i.find_mle_sin(sig_x, drive_freq=freq, noise_rms=noise_rms, bandwidth=100, plot=True, **fit_kwargs)
-
+empirical_scale = 0.18642853643227691
+m1 = lc_i.find_mle_sin(sig_x, drive_freq=freq, noise_rms=noise_rms*empirical_scale, bandwidth=100, plot=False,
+                       suppress_print=True, **fit_kwargs)
--- Tools/StatFramework/likelihood_calculator/likelihood_analyser.py
+++ Tools/StatFramework/likelihood_calculator/likelihood_analyser.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 from iminuit import Minuit
-import pprint
+import LeastSquares
+import time
 
 
 class LikelihoodAnalyser:
@@ -24,10 +25,28 @@ class LikelihoodAnalyser:
         res = sum(np.power(np.abs(self.data_y - func_t), 2))
         return res / self.noise_sigma ** 2
 
-    def find_mle_sin(self, x, drive_freq=0, fsamp=5000, bandwidth=50, noise_rms=0, plot=False, **kwargs):
+    def least_squares_sine2(self, A, f, phi, sigma):
+        """
+        least squares for minimization - sine function
+        This function takes the white noise width as a parameter as well
+        :param sigma: gaussian white noise variance
+        :param A: Amplitude
+        :param f: frequency
+        :param phi: phase
+        :return: cost function - sum of squares
+        """
+        func_t = A * np.sin(2 * np.pi * f * self.data_x + phi)  # function to minimize
+        res = sum(np.power(np.abs(self.data_y - func_t), 2))
+        res += len(self.data_x) * np.log(np.pi * sigma ** 2)
+        return res / sigma ** 2
+
+    def find_mle_sin(self, x, drive_freq=0, fsamp=5000, bandwidth=50, noise_rms=0, plot=False, suppress_print=True,
+                     **kwargs):
         """
         The function is fitting the data with a sine template using iminuit.
         The fitting is done after applying a bandpass filter.
+        :param suppress_print: suppress all printing
+        :param noise_rms: std of the white gaussian noise
         :param plot: plot the data and its fft
         :param bandwidth: bandwidth for butter filter [Hz]
         :param fsamp: sampling rate [1/sec]
@@ -35,25 +54,39 @@ class LikelihoodAnalyser:
         :param drive_freq: drive frequency of the response
         :return: estimated values, chi_square
         """
-        print('Data overall time: ', len(x) / fsamp, ' sec.')
+        if not suppress_print:
+            print('Data overall time: ', len(x) / fsamp, ' sec.')
         self.fsamp = fsamp
         if noise_rms != 0:
             self.noise_sigma = noise_rms
 
-        # apply bandpass filter to data and store data in the correct place for the minimization
+        # apply a bandpass filter to data and store data in the correct place for the minimization
         self.data_x = np.arange(0, len(x)) / fsamp
+        start = time.time()
         if drive_freq != 0:
-            print('Bandpass filter on. Bandwidth: ', bandwidth, 'Hz')
+            if not suppress_print:
+                print('Bandpass filter ON. Bandwidth: ', bandwidth, 'Hz')
             b, a = signal.butter(3, [2. * (drive_freq - bandwidth / 2.) / self.fsamp,
                                      2. * (drive_freq + bandwidth / 2.) / self.fsamp], btype='bandpass')
             self.data_y = signal.filtfilt(b, a, x)
         else:
             self.data_y = x
+        end = time.time()
+        if not suppress_print:
+            print('bandpass time: ', end - start)
 
         # we create an instance of Minuit and pass the function to minimize
-        m = Minuit(self.least_squares_sine, **kwargs)
-        m.migrad(ncall=50000)
-        print(m.get_param_states())
+        if 'sigma' in kwargs.keys():
+            mimuit_minimizer = Minuit(self.least_squares_sine2, **kwargs)
+        else:
+            mimuit_minimizer = Minuit(self.least_squares_sine, **kwargs)
+
+        start = time.time()
+        mimuit_minimizer.migrad(ncall=50000)
+        end = time.time()
+        if not suppress_print:
+            print('minimization time: ', end - start)
+            print(mimuit_minimizer.get_param_states())
 
         if plot:
             _, ax = plt.subplots(1, 2, figsize=(9.5, 4))
@@ -62,9 +95,9 @@ class LikelihoodAnalyser:
             freq = np.fft.rfftfreq(len(x), d=1. / fsamp)
             ax[0].set(title='raw data')
             plt.subplot(122)
-            m.draw_profile('A', subtract_min=True)
+            mimuit_minimizer.draw_profile('A', subtract_min=True)
             plt.show()
+        if not suppress_print:
+            print('reduced chi2: ', mimuit_minimizer.fval / (len(self.data_y) - 2))
 
-        print('reduced chi2: ', m.fval / (len(self.data_y) - 2))
-
-        return m
+        return mimuit_minimizer
